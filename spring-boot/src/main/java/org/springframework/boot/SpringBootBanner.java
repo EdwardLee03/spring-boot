5b3ba6d6d1f5232cc7770f6c6b2ad6a2e46bda9d
dannong <dannong@wacai.com>
2019-12-28 00:16:40 +0800

    [boot] Banner-以编程方式编写横幅，SpringBootBanner-Spring Boot框架广告横幅

--- spring-boot/src/main/java/org/springframework/boot/SpringBootBanner.java
+++ spring-boot/src/main/java/org/springframework/boot/SpringBootBanner.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2012-2018 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package org.springframework.boot;
 
@@ -25,11 +10,15 @@ import org.springframework.core.env.Environment;
 
 /**
  * Default Banner implementation which writes the 'Spring' banner.
+ * 默认的横幅实现，写入Spring广告横幅。
  *
  * @author Phillip Webb
  */
 class SpringBootBanner implements Banner {
 
+	/**
+	 * Spring横幅
+	 */
 	private static final String[] BANNER = { "",
 			"  .   ____          _            __ _ _",
 			" /\\\\ / ___'_ __ _ _(_)_ __  __ _ \\ \\ \\ \\",
@@ -38,16 +27,24 @@ class SpringBootBanner implements Banner {
 			"  '  |____| .__|_| |_|_| |_\\__, | / / / /",
 			" =========|_|==============|___/=/_/_/_/" };
 
+	/**
+	 * Spring Boot框架广告宣传文案
+	 */
 	private static final String SPRING_BOOT = " :: Spring Boot :: ";
 
+	/**
+	 * 表带线尺寸
+	 */
 	private static final int STRAP_LINE_SIZE = 42;
 
 	@Override
 	public void printBanner(Environment environment, Class<?> sourceClass,
 			PrintStream printStream) {
+		// 1.打印横幅
 		for (String line : BANNER) {
 			printStream.println(line);
 		}
+		// Spring Boot框架版本
 		String version = SpringBootVersion.getVersion();
 		version = (version != null) ? " (v" + version + ")" : "";
 		String padding = "";
@@ -56,6 +53,7 @@ class SpringBootBanner implements Banner {
 			padding += " ";
 		}
 
+		// 2.打印Spring Boot广告宣传文案
 		printStream.println(AnsiOutput.toString(AnsiColor.GREEN, SPRING_BOOT,
 				AnsiColor.DEFAULT, padding, AnsiStyle.FAINT, version));
 		printStream.println();
