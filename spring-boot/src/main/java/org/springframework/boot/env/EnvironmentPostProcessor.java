575e3c52d9f113b0ac4c58a20b3fadf29252339d
dannong <dannong@wacai.com>
2019-12-28 18:11:44 +0800

    [boot.env] Spring Environment support/应用运行时环境支持(PropertySourceLoader，EnvironmentPostProcessor)

--- spring-boot/src/main/java/org/springframework/boot/env/EnvironmentPostProcessor.java
+++ spring-boot/src/main/java/org/springframework/boot/env/EnvironmentPostProcessor.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2012-2018 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package org.springframework.boot.env;
 
@@ -23,15 +8,19 @@ import org.springframework.core.env.Environment;
 /**
  * Allows for customization of the application's {@link Environment} prior to the
  * application context being refreshed.
+ * 允许在刷新应用上下文之前，自定义应用运行时环境。
  * <p>
  * EnvironmentPostProcessor implementations have to be registered in
  * {@code META-INF/spring.factories}, using the fully qualified name of this class as the
  * key.
+ * 必须使用这个类的完全限定名称作为关键字，在META-INF/spring.factories中注册应用运行时环境后置处理器实现类。
  * <p>
  * {@code EnvironmentPostProcessor} processors are encouraged to detect whether Spring's
  * {@link org.springframework.core.Ordered Ordered} interface has been implemented or if
  * the {@link org.springframework.core.annotation.Order @Order} annotation is present and
  * to sort instances accordingly if so prior to invocation.
+ * 鼓励应用运行时环境后置处理器检测是否已实现Ordered接口或是否存在@Order注解，
+ * 并在调用之前对实例进行相应排序，如果有。
  *
  * @author Andy Wilkinson
  * @author Stephane Nicoll
@@ -41,8 +30,9 @@ public interface EnvironmentPostProcessor {
 
 	/**
 	 * Post-process the given {@code environment}.
-	 * @param environment the environment to post-process
-	 * @param application the application to which the environment belongs
+	 * 对给定的应用运行时环境进行后置处理。
+	 * @param environment the environment to post-process 后置处理的可配置的应用运行时环境
+	 * @param application the application to which the environment belongs 环境所属的Spring应用对象
 	 */
 	void postProcessEnvironment(ConfigurableEnvironment environment,
 			SpringApplication application);
--- spring-boot/src/main/java/org/springframework/boot/env/package-info.java
+++ spring-boot/src/main/java/org/springframework/boot/env/package-info.java
@@ -1,5 +1,6 @@
 
 /**
  * Spring {@link org.springframework.core.env.Environment} support.
+ * 应用运行时环境支持。
  */
 package org.springframework.boot.env;
